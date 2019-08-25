121be6bd1e63bd0c01761f6a1e4a77bdf4f30006
imlegend19 <mahengandhi19@gmail.com>
2019-08-26 01:04:43 +0530

    Resolved merge conflict by incorporating both suggestions.

--- lib/entity/api_static.dart
+++ lib/entity/api_static.dart
@@ -1,5 +1,5 @@
 abstract class APIStatic {
-  static const baseURL = "http://adminbeta.chakhley.co.in/api/";
+  static const baseURL = "http://admin.chakhley.co.in/api/";
 
   static const keyID = "id";
   static const keyName = "name";
